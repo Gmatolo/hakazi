e250f328d26601fb9198f13ec6cbcf60d4616bf2
gmatolo <mulimatolo@gmail.com>
2023-03-27 15:26:39 +0000

    Adds user resume upload complete page

--- resume/models.py
+++ resume/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Resume(models.Model):
     email = models.EmailField()
-    resume = models.FileField(upload_to='resumes/%Y/%m/%d')
+    resume = models.FileField(upload_to='static/resumes/%Y/%m/%d')
 
     def __str__(self):
         return f'Resume for {self.email}'
