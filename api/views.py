0d76ed0014e4b661ba38027ba57b1bd346d805e7
gmatolo <mulimatolo@gmail.com>
2023-04-01 08:28:40 +0000

    Uodates resume file

--- api/views.py
+++ api/views.py
@@ -1,26 +1,40 @@
-from rest_framework import viewsets
+from django.contrib import messages
+from django.shortcuts import redirect, render
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from  resume.models import Resume
+from resume.forms import EmailForm, ResumeForm
+from .models import Resume
 from .serializers import ResumeSerializer
 
 
-class ResumeViewSet(viewsets.ModelViewSet):
-    queryset = Resume.objects.all()
-    serializer_class = ResumeSerializer
+@api_view(['POST'])
+def get_user_email(request):
+    form = EmailForm(request.data)
+    if form.is_valid():
+        email = form.cleaned_data['email']
+        request.session['email'] = email
+        return Response({'success': True})
+    else:
+        return Response({'success': False, 'errors': form.errors})
+        
 
-    def create(self, request, *args, **kwargs):
-        email = request.data.get('email')
-        if email:
-            request.session['email'] = email
-        return super().create(request, *args, **kwargs)
+@api_view(['POST'])
+def upload_resume(request):
+    email = request.session.get('email')
+    if email:
+        form = ResumeForm(request.data, request.FILES)
+        if form.is_valid():
+            resume = form.save(commit=False)
+            resume.email = email
+            resume.save()
+            del request.session['email']
+            serializer = ResumeSerializer(resume)
+            return Response({'success': True, 'resume': serializer.data})
+        else:
+            return Response({'success': False, 'errors': form.errors})
+    else:
+        return Response({'success': False, 'errors': 'Email not found.'})
 
-    def perform_create(self, serializer):
-        email = self.request.session.get('email')
-        if email:
-            serializer.save(email=email)
-            del self.request.session['email']
-
-    def list(self, request, *args, **kwargs):
-        queryset = self.queryset.filter(email=request.session.get('email'))
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
+@api_view(['GET'])
+def resume_upload_complete(request):
+    return Response({'message': "Resume uploaded successfully."})
