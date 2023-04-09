40b50cf06e7a307f9bf8d13fb74fc8c4c1e6f144
gmatolo <mulimatolo@gmail.com>
2023-04-09 19:16:03 +0000

    Updates degug to false for production

--- hakazi/settings.py
+++ hakazi/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-nh$^*-z2k7=lm@u269i$_g!-1#$ui+^h3lvh()i(vd36_vyn6o"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["www.hakazi.com", "hakazi.com"]
 
 
 # Application definition
