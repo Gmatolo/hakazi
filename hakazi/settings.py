95e007669bf3c9dfc95325432ff22b78db3e451f
gmatolo <mulimatolo@gmail.com>
2023-04-10 08:15:37 +0000

    add localhost in allowed hosts for dev purposes

--- hakazi/settings.py
+++ hakazi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-nh$^*-z2k7=lm@u269i$_g!-1#$ui+^h3lvh()i(vd36_vyn6o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["www.hakazi.com", "hakazi.com"]
+ALLOWED_HOSTS = ["www.hakazi.com", "hakazi.com", "127.0.0.1"]
 
 
 # Application definition
